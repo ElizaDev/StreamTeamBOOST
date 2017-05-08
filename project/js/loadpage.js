2ee865e12bdffe741e01bc619ec4fc76609c2196
Nelson Segarra <junicus@outlook.com>
2017-05-07 20:29:08 -0400

    added back the video iframes

--- project/js/loadpage.js
+++ project/js/loadpage.js
@@ -10,7 +10,7 @@ xhr.onload = function () {
 			return streamer.stream;
 		});
 		//build string with new content (can also use DOM manip
-		
+
 		var html = [];
 		console.log(streamers);
 		//for (var i = 0; i < responseObject.streamers.length; i++) {
@@ -19,12 +19,12 @@ xhr.onload = function () {
 				var newStreamer = '';
 				newStreamer += '<div class="streamers">';
 				newStreamer += '<h1>' + bio.display_name + '</h1>';
-				//				newStreamer += '<iframe src="http://player.twitch.tv/?channel=' + name + '&muted=true"'; 
-				//				newStreamer += ' height="300"'; 
-				//				newStreamer += ' width="400"';
-				//				newStreamer += ' frameborder="0"'; 
-				//				newStreamer += ' scrolling="no"';
-				//				newStreamer += ' allowfullscreen="true"></iframe>';
+				newStreamer += '<iframe src="http://player.twitch.tv/?channel=' + name + '&muted=true"';
+				newStreamer += ' height="300"';
+				newStreamer += ' width="400"';
+				newStreamer += ' frameborder="0"';
+				newStreamer += ' scrolling="no"';
+				newStreamer += ' allowfullscreen="true"></iframe>';
 				//newStreamer += '<iframe src="https://twitch.tv/chat/embed?channel=' + responseObject.streamers[i].stream + '" height="300" width="300"></iframe>';
 				newStreamer += '</div>';
 				html.push(newStreamer);
@@ -38,8 +38,8 @@ xhr.onload = function () {
 function streamBio(user, cb) {
 	console.log('user', user);
 	twitchjson.open('GET', 'https://api.twitch.tv/kraken/users/?login=' + user + '&client_id=<clientid>&api_version=5', false);
-	twitchjson.onreadystatechange = function() {
-		if(twitchjson.readyState === XMLHttpRequest.DONE && twitchjson.status === 200) {
+	twitchjson.onreadystatechange = function () {
+		if (twitchjson.readyState === XMLHttpRequest.DONE && twitchjson.status === 200) {
 			userInfo = JSON.parse(twitchjson.responseText);
 			console.log(userInfo.users[0]);
 			cb(userInfo.users[0]);

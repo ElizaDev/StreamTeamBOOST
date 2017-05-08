a98d8627db7c2de3bbd43a99f43e28377d290f86
Nelson Segarra <junicus@outlook.com>
2017-05-07 20:26:47 -0400

    changed the order of calls to help with the race condition and made it async, it works but not ideal

--- project/js/loadpage.js
+++ project/js/loadpage.js
@@ -2,54 +2,50 @@
 var xhr = new XMLHttpRequest();
 var twitchjson = new XMLHttpRequest();
 
-xhr.onload = function() {
-
-	if(xhr.status === 200) {
+xhr.onload = function () {
+	if (xhr.status === 200) {
 		res = JSON.parse(xhr.responseText).streamers;
 
-		var streamers = res.map(function(streamer) {
+		var streamers = res.map(function (streamer) {
 			return streamer.stream;
 		});
 		//build string with new content (can also use DOM manip
-		var newStreamer = '';
-
+		
+		var html = [];
+		console.log(streamers);
 		//for (var i = 0; i < responseObject.streamers.length; i++) {
-		streamers.forEach(function(name) {
-			streamBio(name, function(bio) {
-				console.log('bio',bio);
+		streamers.forEach(function (name) {
+			streamBio(name, function (bio) {
+				var newStreamer = '';
 				newStreamer += '<div class="streamers">';
-				newStreamer += '<h1>' + bio[0] + '</h1>';
-				newStreamer += '<iframe src="http://player.twitch.tv/?channel=' + name + '&muted=true"'; 
-				newStreamer += ' height="300"'; 
-				newStreamer += ' width="400"';
-				newStreamer += ' frameborder="0"'; 
-				newStreamer += ' scrolling="no"';
-				newStreamer += ' allowfullscreen="true"></iframe>';
+				newStreamer += '<h1>' + bio.display_name + '</h1>';
+				//				newStreamer += '<iframe src="http://player.twitch.tv/?channel=' + name + '&muted=true"'; 
+				//				newStreamer += ' height="300"'; 
+				//				newStreamer += ' width="400"';
+				//				newStreamer += ' frameborder="0"'; 
+				//				newStreamer += ' scrolling="no"';
+				//				newStreamer += ' allowfullscreen="true"></iframe>';
 				//newStreamer += '<iframe src="https://twitch.tv/chat/embed?channel=' + responseObject.streamers[i].stream + '" height="300" width="300"></iframe>';
 				newStreamer += '</div>';
+				html.push(newStreamer);
+				document.getElementById('stream-table').insertAdjacentHTML('beforeEnd', newStreamer);
 			});
 		});
-		document.getElementById('stream-table').insertAdjacentHTML('beforeEnd',newStreamer);
+		console.log(html);
 	}
 };
 
 function streamBio(user, cb) {
-	twitchjson.open('GET', 'https://api.twitch.tv/kraken/users/?login=' + user + '&client_id=<PUT TWITCH API KEY HERE>&api_version=5', true);
-	twitchjson.send();
-
-	twitchjson.addEventListener('readystatechange', processRequest, false);
-	var info = '';
-	function processRequest(e) {
-		if (twitchjson.readyState == 4 && twitchjson.status === 200) {
+	console.log('user', user);
+	twitchjson.open('GET', 'https://api.twitch.tv/kraken/users/?login=' + user + '&client_id=<clientid>&api_version=5', false);
+	twitchjson.onreadystatechange = function() {
+		if(twitchjson.readyState === XMLHttpRequest.DONE && twitchjson.status === 200) {
 			userInfo = JSON.parse(twitchjson.responseText);
-			var name = userInfo.users[0].display_name;
-			var bio =  userInfo.users[0].bio;
-			var logo = userInfo.users[0].logo;
-			var info = [name,bio,logo];
+			console.log(userInfo.users[0]);
+			cb(userInfo.users[0]);
 		}
 	}
-	cb(info);
-	console.log('cb',info);
+	twitchjson.send();
 };
 
 xhr.open('GET', 'js/streamers.json', true);
